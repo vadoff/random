771bde76cd233ab68ab796911607da4b101783b9
Randolph Lee <vadoff@gmail.com>
2013-09-09 22:30:56 -0700

    Create Random.m

---
+++ Random.m
@@ -0,0 +1,35 @@
+//
+//  Created by Randolph Lee on 9/9/13.
+//  Feel free to do anything with this, no credit needed.
+//
+
+#import "Random.h"
+
+@implementation Random
+
++ (NSInteger)randomIntegerFrom:(NSInteger)fromInteger to:(NSInteger)toInteger {
+  if (fromInteger > toInteger) {
+    NSInteger tempInteger = fromInteger;
+    fromInteger = toInteger;
+    toInteger = tempInteger;
+  }
+  
+  NSInteger difference = toInteger - fromInteger;
+  return fromInteger + arc4random() % (difference + 1);
+}
+
++ (CGFloat)randomFloatFrom:(CGFloat)fromFloat to:(CGFloat)toFloat {
+  if (fromFloat > toFloat) {
+    CGFloat tempFloat = fromFloat;
+    fromFloat = toFloat;
+    toFloat = tempFloat;
+  }
+  
+  CGFloat difference = toFloat - fromFloat;
+  
+  CGFloat floata = ((double)arc4random() / 0x100000000) * difference + fromFloat;
+  NSLog(@"%f", floata);
+  return floata;
+}
+
+@end
